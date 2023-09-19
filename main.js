c6d7d1c0e0f871348b07537c13c42ec44d5e5bfd
morasa zadbagher sighalani <100466704+MORASAzb@users.noreply.github.com>
2023-09-19 13:28:59 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,12 @@
-// import './style.css'
-
-
-// const input = prompt('enter your time :');
-// const inputSecond = +input;
-
-// function timeFormat(second) {
-
-//   if (second === 0) {
-//     return "NOW"
-//   }
-
-//   const timeUnits = {
-//     years: 365 * 12 * 30 * 24 * 60 * 60,
-//     month: 30 * 24 * 60 * 60,
-//     day: 24 * 60 * 60,
-//     hour: 60 * 60,
-//     minute: 60,
-//     seconds: 1,
-//   }
-
-
-//   const formattedDuration = [];
-
-//   for (const [unit, unitSeconds] of Object.entries(timeUnits)) {
-
-
-//     if (second >= unitSeconds) {
-//       const unitCount = Math.floor(second / unitSeconds);
-//       second %= unitSeconds;
-//       if (unitCount > 0) {
-//         formattedDuration.push(`${unitCount} ${unit}${unitCount > 1 ? 's' : ''}`);
-//       }
-
-//     }
-//   }
-
-//   if (formattedDuration.length > 1) {
-//     const lastItem = formattedDuration.pop();
-//     formattedDuration.push(`and ${lastItem}`);
-//   }
-
-
-//   return formattedDuration.join(',');
-// }
-
-
-// console.log(timeFormat(inputSecond));
-
-
-
-
 
  const input = prompt('enter your time :');
  const inputSecond = +input;
 
 function formatDuration(seconds) {
+if(seconds < 0){
+ return "unvalid input";
+}
+ 
   if (seconds === 0) {
     return "now";
   }
